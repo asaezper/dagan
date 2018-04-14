1babc81f51027e5bd41c14e9162df7de5286db02
asaezper <alfredosaezperez@gmail.com>
2018-04-14 15:16:00 +0200

    Scripts actualizados

--- scripts/linux-bash/upgrade-dagan.sh
+++ scripts/linux-bash/upgrade-dagan.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+cd /opt/dagan
+
+cp .venv/lib/python*/site-packages/dagan/resources/dagan.db db-bup/$(date +"%Y-%m-%d-%H-%M-%S").db
+cp .venv/lib/python*/site-packages/dagan/resources/dagan.db /tmp/dagan.db
 .venv/bin/pip uninstall -y dagan
 .venv/bin/pip install dagan*.whl
+cp /tmp/dagan.db .venv/lib/python*/site-packages/dagan/resources/dagan.db
+mv dagan*.whl dist/
